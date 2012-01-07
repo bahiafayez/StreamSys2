f25e7ce6712fb90825cd790ae9c528324841b141
Bahia ElSharkawy <bahia.sharkawy@gmail.com>
2012-01-07 19:20:53 +0200

    adjustedActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 StreamSys2::Application.routes.draw do
   
+  
+
+  ActiveAdmin.routes(self)
+
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   get "users/new"
@@ -19,8 +23,6 @@ StreamSys2::Application.routes.draw do
   
 
   root :to => "pages#home"  
-
-  ActiveAdmin.routes(self)
 
   
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111222110303) do
+ActiveRecord::Schema.define(:version => 20120107191220) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -57,9 +57,6 @@ ActiveRecord::Schema.define(:version => 20111222110303) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
-  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
   create_table "ads", :force => true do |t|
     t.text     "index_file"
@@ -122,8 +119,6 @@ ActiveRecord::Schema.define(:version => 20111222110303) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "resulting_streams", :force => true do |t|
     t.text     "index_file"
